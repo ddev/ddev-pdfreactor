4e5b1bf642b0d64ab3011883a4bbb3581952a643
Sebastian Blank <blank@data-factory.net>
2022-07-26 09:39:14 +0200

    Review

--- tests/testdata/test.php
+++ tests/testdata/test.php
@@ -12,34 +12,34 @@ date_default_timezone_set('CET');
 
 $pdfReactor = new PDFreactor('http://pdfreactor:9423/service/rest');
 
-$config = array(
+$config = [
     'document' => $content,
     'logLevel' => LogLevel::DEBUG,
     'title' => 'Demonstration of PDFreactor PHP API',
     'author'=> 'Myself',
-    'viewerPreferences' => array(
+    'viewerPreferences' => [
         ViewerPreferences::FIT_WINDOW,
-        ViewerPreferences::PAGE_MODE_USE_THUMBS
-    ),
-    'userStyleSheets' => array(
-        array(
+        ViewerPreferences::PAGE_MODE_USE_THUMBS,
+    ],
+    'userStyleSheets' => [
+        [
             'content'=> '@page {' .
-                            '@top-center {'.
-                                'content: "PDFreactor PHP API demonstration";'.
-                            '}'.
-                            '@bottom-center {' .
-                                'content: "Created on '.date('m/d/Y G:i:s A').'";' .
-                            '}' .
-                        '}'
-        ),
-        array( 'uri'=> 'resources/common.css' )
-    )
-);
+                '@top-center {'.
+                    'content: "PDFreactor PHP API demonstration";'.
+                '}'.
+                '@bottom-center {' .
+                    'content: "Created on '.date('m/d/Y G:i:s A').'";' .
+                '}' .
+            '}',
+        ],
+        ['uri'=> 'resources/common.css'],
+    ],
+];
 
 $result = $pdfReactor->convertAsBinary($config);
 
 if (preg_match('/^%PDF-/', $result)) {
-    echo 'PDF generation was successful';
+    echo 'PDF generation was successful' . "\n";
 } else {
     throw new \Exception('Generated PDF file is invalid');
 }
